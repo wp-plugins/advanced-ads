3fb34840ffd197a1eea2db4e18893f0d8f9d09f3
webzunft <webzunft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-13 07:39:53 +0000

    version 1.4.8
    git-svn-id: https://plugins.svn.wordpress.org/advanced-ads/tags/1.4.8@1133532 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/ad_group.php
+++ classes/ad_group.php
@@ -18,340 +18,386 @@
  */
 class Advads_Ad_Group {
 
-    /**
-     * default ad group weight
-     */
-    const MAX_AD_GROUP_WEIGHT = 10;
+	/**
+	 * default ad group weight
+	 */
+	const MAX_AD_GROUP_WEIGHT = 10;
 
-    /**
-     * id of the taxonomy of this ad group
-     */
-    public $id = 0;
+	/**
+	 * term id of this ad group
+	 */
+	public $id = 0;
 
-    /**
-     * name of the taxonomy
-     */
-    protected $taxonomy = '';
+	/**
+	 * group type
+         *
+         * @since 1.4.8
+	 */
+	public $type = 'default';
 
-    /**
-     * post type of the ads
-     */
-    protected $post_type = '';
+	/**
+	 * name of the taxonomy
+	 */
+	public $taxonomy = '';
 
-    /**
-     * the current loaded ad
-     */
-    protected $current_ad = '';
+	/**
+	 * post type of the ads
+	 */
+	protected $post_type = '';
 
-    /**
-     * the name of the term
-     */
-    public $name = '';
+	/**
+	 * the current loaded ad
+	 */
+	protected $current_ad = '';
 
-    /**
-     * the slug of the term
-     */
-    public $slug = '';
+	/**
+	 * the name of the term
+	 */
+	public $name = '';
 
-    /**
-     * the description of the term
-     */
-    public $description = '';
+	/**
+	 * the slug of the term
+	 */
+	public $slug = '';
 
-    /**
-     * containing ad weights
-     */
-    private $ad_weights = 0;
+	/**
+	 * the description of the term
+	 */
+	public $description = '';
 
-    /**
-     * array with post type objects (ads)
-     */
-    private $ads = array();
+	/**
+	 * number of ads to display in the group block
+	 */
+	public $ad_count = 1;
 
-    /**
-     * init ad group object
-     *
-     * @since 1.0.0
-     * @param int $id id of the ad group (= taxonomy id)
-     */
-    public function __construct($id) {
-        $id = absint($id);
+	/**
+	 * containing ad weights
+	 */
+	private $ad_weights = 0;
 
-        if (empty($id))
-            return;
+	/**
+	 * array with post type objects (ads)
+	 */
+	private $ads = array();
 
-        $this->id = $id;
-        $this->taxonomy = Advanced_Ads::AD_GROUP_TAXONOMY;
-        $this->post_type = Advanced_Ads::POST_TYPE_SLUG;
+	/**
+	 * init ad group object
+	 *
+	 * @since 1.0.0
+	 * @param int|obj $group either id of the ad group (= taxonomy id) or term object
+	 */
+	public function __construct($group) {
 
-        $this->load($id);
-    }
+		$this->taxonomy = Advanced_Ads::AD_GROUP_TAXONOMY;
 
-    /**
-     * load an ad group object by id
-     *
-     * @since 1.0.0
-     */
-    private function load($id = 0) {
-        $_group = get_term($id, $this->taxonomy);
-        if ($_group == null)
-            return;
+                $group = get_term( $group, $this->taxonomy );
+		if ( $group == null || is_wp_error($group) ) { return; }
 
-        $this->name = $_group->name;
-        $this->slug = $_group->slug;
-        $this->description = $_group->description;
-    }
+		$this->load( $group );
+	}
 
-    /**
-     * return random ad content for frontend output
-     *
-     * @since 1.0.0
-     * @return string ad content
-     */
-    public function output_random_ad() {
-        // see prepare_frontend_output for example for this filter
-        $ad = $this->get_random_ad();
+	/**
+	 * load additional ad group properties
+         *
+	 * @since 1.4.8
+         * @param int $id group id
+         * @param obj $group wp term object
+	 */
+	private function load($group) {
+            $this->id = $group->term_id;
+            $this->name = $group->name;
+            $this->slug = $group->slug;
+            $this->description = $group->description;
+            $this->post_type = Advanced_Ads::POST_TYPE_SLUG;
 
-        if (!is_object($ad))
-            return '';
+            $this->load_additional_attributes();
+	}
 
-        // makes sure the ad filters can also run here
-        $adcontent = $ad->output();
+        /**
+         * load additional attributes for groups that are not part of the WP terms
+         *
+         * @since 1.4.8
+         */
+        protected function load_additional_attributes(){
+            $all_groups = get_option( 'advads-ad-groups', array() );
 
-        // filter again, in case a developer wants to filter group output individually
-        $output = apply_filters('advanced-ads-group-output', $adcontent, $this);
+            if(isset($all_groups[$this->id]['type'])){
+                $this->type = $all_groups[$this->id]['type'];
+            }
 
-        return $output;
-    }
-
-    /**
-     * get a random ad from this group
-     *
-     * @since 1.0.0
-     * @return
-     */
-    private function get_random_ad() {
-
-        // load all ads
-        $ads = $this->load_all_ads();
-
-        // return, if no ads given
-        if ($ads === array())
-            return;
-
-        // shuffle ads based on ad weight
-        $ads = $this->shuffle_ads($ads);
-
-        // check ads one by one for being able to be displayed on this spot
-        foreach ($ads as $_ad) {
-            // load the ad object
-            $ad = new Advads_Ad($_ad->ID);
-            if ($ad->can_display()) {
-                return $ad;
+            // get ad count; default is 1
+            if(isset($all_groups[$this->id]['ad_count'])){
+                $this->ad_count = (int) $all_groups[$this->id]['ad_count'];
             }
         }
 
-        return '';
-    }
+        /**
+         * control the output of the group by type and amount of ads
+         *
+         * @since 1.4.8
+         * @return str $output output of ad(s) by ad
+         */
+        public function output(){
 
-    /**
-     * return all ads from this group
-     *
-     * @since 1.0.0
-     */
-    public function get_all_ads() {
-        if(count($this->ads) > 0)
-            return $this->ads;
-        else
-            return $this->load_all_ads();
-    }
+            if(!$this->id) return;
 
-    /**
-     * load all public ads for this group
-     *
-     * @since 1.0.0
-     * @update 1.1.0 load only public ads
-     * @return arr $ads array with ad (post) objects
-     */
-    private function load_all_ads() {
+            // load ads
+            $ads = $this->load_all_ads();
+            if ( $ads === array() ) { return; }
 
-        $args = array(
-            'post_type' => $this->post_type,
-            'post_status' => 'publish',
-            'taxonomy' => $this->taxonomy,
-            'term' => $this->slug,
-            'orderby' => 'id'
-        );
-        $ads = new WP_Query($args);
+            // get ad weights serving as a order here
+            $weights = $this->get_ad_weights();
+            asort($weights);
 
-        if ($ads->have_posts()) {
-            return $this->ads = $this->add_post_ids($ads->posts);
-        } else {
-            return $this->ads = array();
-        }
-    }
-
-    /**
-     * use post ids as keys for ad array
-     *
-     * @since 1.0.0
-     * @param arr $ads array with post objects
-     * @return arr $ads array with post objects with post id as their key
-     * @todo check, if there isn’t a WP function for this already
-     */
-    private function add_post_ids(array $ads){
-
-        $ads_with_id = array();
-        foreach($ads as $_ad){
-            $ads_with_id[$_ad->ID] = $_ad;
-        }
-
-        return $ads_with_id;
-    }
-
-    /**
-     * shuffle ads based on ad weight
-     *
-     * @since 1.0.0
-     * @param arr $ads array with ad objects
-     * @return arr $shuffled_ads shuffled array with ad objects
-     */
-    private function shuffle_ads($ads = array()) {
-
-        // get saved ad weights
-        $weights = $this->get_ad_weights();
-
-        // if ads and weights don’t have the same keys, update weights array
-        if((count($weights) == 0 && count($ads) > 0) || count($weights) != count($ads) || array_diff_key($weights, $ads) != array()
-                || array_diff_key($ads, $weights) != array()) {
-            $this->update_ad_weights();
-        }
-
-        // get a random ad for every ad there is
-        $shuffled_ads = array();
-        // order array by ad weight; lowest first
-        asort($weights);
-        // while non-zero weights are set select random next
-        while (null !== $random_ad_id = $this->get_random_ad_by_weight($weights)) {
-            // remove chosen ad from weights array
-            unset($weights[$random_ad_id]);
-            // put random ad into shuffled array
-            $shuffled_ads[] = $ads[$random_ad_id];
-        }
-
-        return $shuffled_ads;
-    }
-
-    /**
-     * get random ad by ad weight
-     *
-     * @since 1.0.0
-     * @param array $ad_weights e.g. array(A => 2, B => 3, C => 5)
-     * @source applied with fix for order http://stackoverflow.com/a/11872928/904614
-     */
-    private function get_random_ad_by_weight(array $ad_weights) {
-
-        // use maximum ad weight for ads without this
-        // ads might have a weight of zero (0); to avoid mt_rand fail assume that at least 1 is set.
-        $max = array_sum($ad_weights);
-        if ($max < 1) {
-            return ;
-        }
-
-        $rand = mt_rand(1, $max);
-
-        foreach ($ad_weights as $key => $value) {
-            $rand -= $value;
-            if ($rand <= 0) {
-                return $key;
+            // if ads and weights don’t have the same keys, update weights array
+            if ( (count( $weights ) == 0 && count( $ads ) > 0) || count( $weights ) != count( $ads ) || array_diff_key( $weights, $ads ) != array()
+                    || array_diff_key( $ads, $weights ) != array() ) {
+                $this->update_ad_weights();
+                $weights = $this->ad_weights;
             }
-        }
-    }
 
-    /**
-     * get weights of ads in this group
-     *
-     * @since 1.0.0
-     */
-    public function get_ad_weights() {
-        // load and save ad weights if not yet set
-        if ($this->ad_weights == 0) {
-            $weights = get_option('advads-ad-weights', array());
-            if (isset($weights[$this->id])) {
-                return $this->ad_weights = $weights[$this->id];
+            // order ads based on group type
+            switch($this->type){
+                case 'ordered' :
+                    $ordered_ad_ids = array_keys($weights);
+                    break;
+                default : // default
+                    $ordered_ad_ids = $this->shuffle_ads($ads, $weights);
             }
-        } elseif(is_array($this->ad_weights)) { // return ad weigths if not empty
-            return $this->ad_weights;
-        }
 
-        // return empty array
-        return array();
-    }
-
-    /**
-     * save ad group weight (into global ad weight array)
-     *
-     * @since 1.0.0
-     * @param arr|str $weights array with ad weights (key: ad id; value: weight)
-     */
-    public function save_ad_weights($weights = '') {
-
-        // allow only arrays and empty string
-        if (!is_array($weights) && $weights = '')
-            return;
-
-        $global_weights = get_option('advads-ad-weights', array());
-
-        $global_weights[$this->id] = $this->sanitize_ad_weights($weights);
-
-        update_option('advads-ad-weights', $global_weights);
-
-        // refresh ad weights after update to avoid conflict
-        $this->ad_weights = $global_weights[$this->id];
-    }
-
-    /**
-     * update ad weight based on current ads for the group and ad weight
-     *
-     * @since 1.0.0
-     */
-    private function update_ad_weights(){
-        $ads = $this->get_all_ads();
-        $weights = $this->get_ad_weights();
-
-        $new_weights = array();
-        // use only ads assigned to the group
-        foreach($ads as $_ad){
-            if(isset($weights[$_ad->ID])){
-                $new_weights[$_ad->ID] = $weights[$_ad->ID];
-            } else {
-                // if no weight is given, use maximum default value
-                $new_weights[$_ad->ID] = self::MAX_AD_GROUP_WEIGHT;
+            // load the ad outpur
+            $output = '';
+            $ads_displayed = 0;
+            foreach ( $ordered_ad_ids as $_ad_id ) {
+                // load the ad object
+                $ad = new Advads_Ad( $_ad_id );
+                if ( $ad->can_display() ) {
+                    $output .= $ad->output();
+                    $ads_displayed++;
+                    if($ads_displayed === $this->ad_count) break;
+                }
+                // break the loop when maximum ads are reached
             }
+
+            // add the group to the global output array
+            $advads = Advanced_Ads::get_instance();
+            $advads->current_ads[] = array('type' => 'group', 'id' => $this->id, 'title' => $this->name);
+
+            // filter again, in case a developer wants to filter group output individually
+            return apply_filters( 'advanced-ads-group-output', $output, $this );
         }
 
-        $this->save_ad_weights($new_weights);
-    }
+	/**
+	 * return all ads from this group
+	 *
+	 * @since 1.0.0
+	 */
+	public function get_all_ads() {
+		if ( count( $this->ads ) > 0 ) {
+			return $this->ads; }
+		else {
+			return $this->load_all_ads(); }
+	}
 
-    /**
-     * sanitize ad weights
-     *
-     * @since 1.0.0
-     * @param arr $weights ad weights array with (key: ad id; value: weight)
-     */
-    private function sanitize_ad_weights($weights = array()) {
+	/**
+	 * load all public ads for this group
+	 *
+	 * @since 1.0.0
+	 * @update 1.1.0 load only public ads
+	 * @return arr $ads array with ad (post) objects
+	 */
+	private function load_all_ads() {
 
-        if (!is_array($weights))
-            return '';
+            if(!$this->id) return array();
 
-        $sanitized_weights = array();
-        foreach ($weights as $_ad_id => $_weight) {
-            $_ad_id = absint($_ad_id);
-            $_weight = absint($_weight);
-            $sanitized_weights[$_ad_id] = $_weight;
-        }
+		$args = array(
+			'post_type' => $this->post_type,
+			'post_status' => 'publish',
+			'posts_per_page' => -1,
+			'taxonomy' => $this->taxonomy,
+			'term' => $this->slug,
+			'orderby' => 'id'
+		);
+		$ads = new WP_Query( $args );
 
-        return $sanitized_weights;
-    }
+		if ( $ads->have_posts() ) {
+			return $this->ads = $this->add_post_ids( $ads->posts );
+		} else {
+			return $this->ads = array();
+		}
+	}
+
+	/**
+	 * use post ids as keys for ad array
+	 *
+	 * @since 1.0.0
+	 * @param arr $ads array with post objects
+	 * @return arr $ads array with post objects with post id as their key
+	 * @todo check, if there isn’t a WP function for this already
+	 */
+	private function add_post_ids(array $ads){
+
+		$ads_with_id = array();
+		foreach ( $ads as $_ad ){
+			$ads_with_id[$_ad->ID] = $_ad;
+		}
+
+		return $ads_with_id;
+	}
+
+	/**
+	 * shuffle ads based on ad weight
+	 *
+	 * @since 1.0.0
+	 * @param arr $ads array with ad objects
+	 * @param arr $weights ad weights
+	 * @return arr $shuffled_ads shuffled array with ad ids
+	 */
+	private function shuffle_ads($ads = array(), $weights) {
+
+		// get a random ad for every ad there is
+		$shuffled_ads = array();
+		// while non-zero weights are set select random next
+		while ( null !== $random_ad_id = $this->get_random_ad_by_weight( $weights ) ) {
+			// remove chosen ad from weights array
+			unset($weights[$random_ad_id]);
+			// put random ad into shuffled array
+			if ( ! empty($ads[$random_ad_id]) ) {
+				$shuffled_ads[] = $random_ad_id; }
+		}
+
+		return $shuffled_ads;
+	}
+
+	/**
+	 * get random ad by ad weight
+	 *
+	 * @since 1.0.0
+	 * @param array $ad_weights e.g. array(A => 2, B => 3, C => 5)
+	 * @source applied with fix for order http://stackoverflow.com/a/11872928/904614
+	 */
+	private function get_random_ad_by_weight(array $ad_weights) {
+
+		// use maximum ad weight for ads without this
+		// ads might have a weight of zero (0); to avoid mt_rand fail assume that at least 1 is set.
+		$max = array_sum( $ad_weights );
+		if ( $max < 1 ) {
+			return ;
+		}
+
+		$rand = mt_rand( 1, $max );
+
+		foreach ( $ad_weights as $key => $value ) {
+			$rand -= $value;
+			if ( $rand <= 0 ) {
+				return $key;
+			}
+		}
+	}
+
+	/**
+	 * get weights of ads in this group
+	 *
+	 * @since 1.0.0
+	 */
+	public function get_ad_weights() {
+		// load and save ad weights if not yet set
+		if ( $this->ad_weights == 0 ) {
+			$weights = get_option( 'advads-ad-weights', array() );
+			if ( isset($weights[$this->id]) ) {
+				return $this->ad_weights = $weights[$this->id];
+			}
+		} elseif ( is_array( $this->ad_weights ) ) { // return ad weigths if not empty
+			return $this->ad_weights;
+		}
+
+		// return empty array
+		return array();
+	}
+
+        /**
+	 * save ad group information that are not included in terms or ad weight
+	 *
+	 * @since 1.4.8
+	 * @param arr $args group arguments
+	 */
+	public function save($args = array()) {
+
+            $defaults = array( 'type' => 'default', 'ad_count' => 1 );
+            $args = wp_parse_args($args, $defaults);
+
+            // get global ad group option
+            $groups = get_option( 'advads-ad-groups', array() );
+
+            $groups[$this->id] = $args;
+
+            update_option( 'advads-ad-groups', $groups );
+	}
+
+	/**
+	 * save ad group weight (into global ad weight array)
+	 *
+	 * @since 1.0.0
+	 * @param arr|str $weights array with ad weights (key: ad id; value: weight)
+	 */
+	public function save_ad_weights($weights = '') {
+
+		// allow only arrays and empty string
+		if ( ! is_array( $weights ) && $weights = '' ) {
+			return; }
+
+		$global_weights = get_option( 'advads-ad-weights', array() );
+
+		$global_weights[$this->id] = $this->sanitize_ad_weights( $weights );
+
+		update_option( 'advads-ad-weights', $global_weights );
+
+		// refresh ad weights after update to avoid conflict
+		$this->ad_weights = $global_weights[$this->id];
+	}
+
+	/**
+	 * update ad weight based on current ads for the group and ad weight
+	 *
+	 * @since 1.0.0
+	 */
+	private function update_ad_weights(){
+		$ads = $this->get_all_ads();
+		$weights = $this->get_ad_weights();
+
+		$new_weights = array();
+		// use only ads assigned to the group
+		foreach ( $ads as $_ad ){
+			if ( isset($weights[$_ad->ID]) ){
+				$new_weights[$_ad->ID] = $weights[$_ad->ID];
+			} else {
+				// if no weight is given, use maximum default value
+				$new_weights[$_ad->ID] = self::MAX_AD_GROUP_WEIGHT;
+			}
+		}
+
+		$this->save_ad_weights( $new_weights );
+	}
+
+	/**
+	 * sanitize ad weights
+	 *
+	 * @since 1.0.0
+	 * @param arr $weights ad weights array with (key: ad id; value: weight)
+	 */
+	private function sanitize_ad_weights($weights = array()) {
+
+		if ( ! is_array( $weights ) ) {
+			return ''; }
+
+		$sanitized_weights = array();
+		foreach ( $weights as $_ad_id => $_weight ) {
+			$_ad_id = absint( $_ad_id );
+			$_weight = absint( $_weight );
+			$sanitized_weights[$_ad_id] = $_weight;
+		}
+
+		return $sanitized_weights;
+	}
 
 }
---
+++ modules/gadsense/includes/class-gadsense-data.php
@@ -0,0 +1,76 @@
+<?php
+
+class Gadsense_Data {
+
+	private static $instance;
+
+	private $options;
+
+	private $resizing;
+
+	private function __construct() {
+		$options = get_option( GADSENSE_OPT_NAME, array() );
+		$update = false;
+
+		// adSense publisher id
+		if ( ! isset($options['adsense_id']) ) {
+			$options['adsense_id'] = '';
+			$update = true;
+		}
+		// Limit of 3 ads per page
+		if ( ! isset($options['limit_ads_per_page']) ) {
+			$options['limit_ads_per_page'] = true;
+			$update = true;
+		}
+
+		if ( $update ) {
+			update_option( GADSENSE_OPT_NAME, $options );
+		}
+		$this->options = $options;
+
+		// Resizing method for responsive ads
+		$this->resizing = array(
+			'auto' => __( 'Auto', ADVADS_SLUG ),
+		);
+	}
+
+	/**
+	 * SETTERS
+	 */
+	public function set_adsense_id($id) {
+		$old_id = $this->options['adsense_id'];
+		$this->options['adsense_id'] = $id;
+		update_option( GADSENSE_OPT_NAME, $this->options );
+		do_action( 'advanced-ads-gadsense-after-id-changed', $id, $old_id );
+	}
+
+	public function set_limit_per_page($value = true) {
+		$this->options['limit_ads_per_page'] = $value;
+		update_option( GADSENSE_OPT_NAME, $this->options );
+	}
+
+	/**
+	 * GETTERS
+	 */
+	public function get_adsense_id() {
+		return $this->options['adsense_id'];
+	}
+
+	public function get_limit_per_page() {
+		return $this->options['limit_ads_per_page'];
+	}
+
+	public function get_responsive_sizing() {
+		$resizing = $this->resizing;
+		$this->resizing = apply_filters( 'advanced-ads-gadsense-responsive-sizing', $resizing );
+		return $this->resizing;
+	}
+
+	public static function get_instance() {
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+		return self::$instance;
+	}
+
+}
