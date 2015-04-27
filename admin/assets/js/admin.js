0eef25eeec3a239070e480297c93a1c0b025f072
webzunft <webzunft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-27 09:30:41 +0000

    version 1.5.1.
    git-svn-id: https://plugins.svn.wordpress.org/advanced-ads/tags/1.5.1@1146843 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -1,102 +1,220 @@
-jQuery(document).ready(function ($) {
-    "use strict";
+jQuery( document ).ready(function ($) {
+	"use strict";
 
-    function advads_load_ad_type_parameter_metabox(ad_type) {
-        $('#advanced-ads-ad-parameters').html('<span class="spinner advads-ad-parameters-spinner"></span>');
-        $.ajax({
-            type: 'POST',
-            url: ajaxurl,
-            data: {
-                'action': 'load_ad_parameters_metabox',
-                'ad_type': ad_type,
-                'ad_id': $('#post_ID').val()
-            },
-            success: function (data, textStatus, XMLHttpRequest) {
-                // toggle main content field
-                if (data) {
-                    $('#advanced-ads-ad-parameters').html(data);
-                }
-            },
-            error: function (MLHttpRequest, textStatus, errorThrown) {
-                $('#advanced-ads-ad-parameters').html(errorThrown);
+	function advads_load_ad_type_parameter_metabox(ad_type) {
+		$( '#advanced-ads-ad-parameters' ).html( '<span class="spinner advads-ad-parameters-spinner"></span>' );
+		$.ajax({
+			type: 'POST',
+			url: ajaxurl,
+			data: {
+				'action': 'load_ad_parameters_metabox',
+				'ad_type': ad_type,
+				'ad_id': $( '#post_ID' ).val()
+			},
+			success: function (data, textStatus, XMLHttpRequest) {
+				// toggle main content field
+				if (data) {
+					$( '#advanced-ads-ad-parameters' ).html( data ).trigger( 'paramloaded' );
+				}
+			},
+			error: function (MLHttpRequest, textStatus, errorThrown) {
+				$( '#advanced-ads-ad-parameters' ).html( errorThrown );
+			}
+		});
+	}
+	;
+
+	$( document ).on('change', '#advanced-ad-type input', function () {
+		var ad_type = $( this ).val()
+		advads_load_ad_type_parameter_metabox( ad_type );
+	});
+
+	// toggle display conditions
+	$( '#advanced-ad-conditions-enable input' ).click(function () {
+		advads_toggle_display_conditions( this.value );
+	});
+	// display on load
+	advads_toggle_display_conditions( $( '#advanced-ad-conditions-enable input:checked' ).val() );
+
+	// display / hide options if all-option is checked for display condition
+	$( '.advanced-ad-display-condition .advads-conditions-all input' ).click(function () {
+		advads_toggle_single_display_conditions( this );
+	});
+	// display / hide options if all-option is checked for display condition – on load
+	$( '.advanced-ad-display-condition .advads-conditions-all input' ).each(function () {
+		advads_toggle_single_display_conditions( this );
+	});
+
+	// toggle single display condition checkboxes that have a counterpart
+	$(document).on('click', '.advads-conditions-single input[type="checkbox"]', function () {
+                advads_toggle_single_display_condition_checkbox( this );
+                // update buttons
+                $('.advads-conditions-terms-buttons label').button('refresh');
+	});
+	// toggle single display condition checkboxes that have a counterpart on load
+	$( '.advads-conditions-single input[type="checkbox"]' ).each(function () {
+		advads_toggle_single_display_condition_checkbox( this );
+	});
+	// activate buttons
+	$('.advads-conditions-terms-buttons').buttonset();
+
+	$(document).on('click', '.advads-conditions-terms-buttons .button', function (e) {
+		$(this).remove();
+	});
+	// display input field to search for terms
+	$( '.advads-conditions-terms-show-search' ).click(function (e) {
+		e.preventDefault();
+		// display input field
+		$(this).siblings('.advads-conditions-terms-search').show().focus();
+		$(this).next('br').show();
+		$(this).hide();
+	});
+	// register autocomplete to display condition posts
+        var response = [];
+	if($( ".advads-conditions-terms-search" ).length){
+		$( ".advads-conditions-terms-search" ).each(function(){
+                    var self = this;
+                    $(this).autocomplete({
+			source: function(request, callback){
+				// var searchField  = request.term;
+				advads_term_search(self, callback);
+			},
+			minLength: 1,
+			select: function( event, ui ) {
+				// append new line with input fields
+				$( '<label class="button">' + ui.item.label + '<input type="hidden" name="advanced_ad[conditions]['+ self.dataset.group +']['+ self.dataset.includeType +'][]" value="' + ui.item.value + '"></label>' ).appendTo( $(self).siblings('.advads-conditions-terms-buttons'));
+
+				// show / hide other elements
+				// $( '#advads-display-conditions-individual-post' ).hide();
+				// $( '.advads-conditions-postids-list .show-search a' ).show();
+			},
+			close: function( event, ui ) {
+                                $(self).val( '' );
+			}
+                    });
+		});
+	};
+	// display input field to search for post, page, etc.
+	$( '.advads-conditions-postids-list .show-search a' ).click(function (e) {
+		e.preventDefault();
+		// display input field
+		$( '#advads-display-conditions-individual-post' ).show();
+		$( this ).hide();
+	});
+	// register autocomplete to display condition posts
+	var response = [];
+	if($( "#advads-display-conditions-individual-post" ).length){
+		$( "#advads-display-conditions-individual-post" ).autocomplete({
+			source: function(request, callback){
+				var searchParam  = request.term;
+				advads_post_search( searchParam, callback );
+			},
+			minLength: 2,
+			select: function( event, ui ) {
+				// append new line with input fields
+				var newline = $( '<li></li>' );
+				$( '<a class="remove" href="#">remove</a>' ).appendTo( newline );
+				$( '<span>' + ui.item.label + '</span><input type="hidden" name="advanced_ad[conditions][postids][ids][]" value="' + ui.item.value + '">' ).appendTo( newline );
+				newline.insertBefore( '.advads-conditions-postids-list .show-search' );
+
+				// show / hide other elements
+				$( '#advads-display-conditions-individual-post' ).hide();
+				$( '.advads-conditions-postids-list .show-search a' ).show();
+			},
+			close: function( event, ui ) {
+				$( '#advads-display-conditions-individual-post' ).val( '' );
+			}
+		});
+	};
+
+	// remove individual posts from the display conditions post list
+	$( document ).on('click', '.advads-conditions-postids-list .remove', function(e){
+		e.preventDefault();
+		$( this ).parent( 'li' ).remove();
+	});
+
+        // display ad groups form
+        $('#advads-ad-group-list a.edit, #advads-ad-group-list a.row-title').click(function(e){
+            e.preventDefault();
+            var advadsgroupformrow = $(this).parents('.advads-group-row').next('.advads-ad-group-form');
+            if(advadsgroupformrow.is(':visible')){
+                advadsgroupformrow.hide();
+            } else {
+                advadsgroupformrow.show();
             }
         });
-    }
-    ;
+        // display ad groups usage
+        $('#advads-ad-group-list a.usage').click(function(e){
+            e.preventDefault();
+            var usagediv = $(this).parents('.advads-group-row').find('.advads-usage');
+            if(usagediv.is(':visible')){
+                usagediv.hide();
+            } else {
+                usagediv.show();
+            }
+        });
+        // menu tabs
+        $('#advads-tabs').find('a').click(function () {
+            $('#advads-tabs').find('a').removeClass('nav-tab-active');
+            $('.advads-tab').removeClass('active');
 
-    $(document).on('change', '#advanced-ad-type input', function () {
-        var ad_type = $(this).val()
-        advads_load_ad_type_parameter_metabox(ad_type);
-    });
+            var id = jQuery(this).attr('id').replace('-tab', '');
+            jQuery('#' + id).addClass('active');
+            jQuery(this).addClass('nav-tab-active');
+        });
 
-    // empty / clear input condition fields in the same row as the clear button
-    $('#advanced-ad-conditions .clear-radio').click(function () {
-        $(this).closest('tr').find('input[type="radio"]').prop('checked', false);
-        $(this).closest('tr').find('input[type="text"]').val('');
-    });
-
-    // toggle display conditions
-    $('#advanced-ad-conditions-enable input').click(function () {
-        advads_toggle_display_conditions(this.value);
-    });
-    // display on load
-    advads_toggle_display_conditions($('#advanced-ad-conditions-enable input:checked').val());
-
-    // display / hide options if all-option is checked for display condition
-    $('.advanced-ad-display-condition .advads-conditions-all input').click(function () {
-        advads_toggle_single_display_conditions(this);
-    });
-    // display / hide options if all-option is checked for display condition – on load
-    $('.advanced-ad-display-condition .advads-conditions-all input').each(function () {
-        advads_toggle_single_display_conditions(this);
-    });
-
-    // toggle single display condition checkboxes that have a counterpart
-    $('.advads-conditions-single input[type="checkbox"]').click(function () {
-        advads_toggle_single_display_condition_checkbox(this);
-    });
-    // toggle single display condition checkboxes that have a counterpart on load
-    $('.advads-conditions-single input[type="checkbox"]').each(function () {
-        advads_toggle_single_display_condition_checkbox(this);
-    });
-    // display input field to search for post, page, etc.
-    $('.advads-conditions-postids-list .show-search a').click(function (e) {
-        e.preventDefault();
-        // display input field
-        $('#advads-display-conditions-individual-post').show();
-        $(this).hide();
-    });
-    // register autocomplete to display condition posts
-    var response = [];
-    $("#advads-display-conditions-individual-post").autocomplete({
-        source: function(request, callback){
-            var searchParam  = request.term;
-            advads_post_search(searchParam, callback);
-        },
-        minLength: 2,
-        select: function( event, ui ) {
-            // append new line with input fields
-            var newline = $('<li></li>');
-            $('<a class="remove" href="#">remove</a>').appendTo(newline);
-            $('<span>' + ui.item.label + '</span><input type="hidden" name="advanced_ad[conditions][postids][ids][]" value="'+ ui.item.value +'">').appendTo(newline);
-            newline.insertBefore('.advads-conditions-postids-list .show-search');
-
-            // show / hide other elements
-            $('#advads-display-conditions-individual-post').hide();
-            $('.advads-conditions-postids-list .show-search a').show();
-        },
-        close: function( event, ui ) {
-            $('#advads-display-conditions-individual-post').val('');
+        // activate specific or first tab
+        var active_tab = window.location.hash.replace('#top#', '');
+        if (active_tab == '' || active_tab == '#_=_') {
+            active_tab = $('.advads-tab').attr('id');
         }
-    });
+        $('#' + active_tab).addClass('active');
+        $('#' + active_tab + '-tab').addClass('nav-tab-active');
+        $('.nav-tab-active').click();
 
-    // remove individual posts from the display conditions post list
-    $(document).on('click', '.advads-conditions-postids-list .remove', function(e){
-        e.preventDefault();
-        $(this).parent('li').remove();
-    });
-
+        /**
+         * SETTINGS PAGE
+         */
+        // render button sets on settings page
+        $(function() {
+            $( ".advads-settings-buttonset" ).buttonset();
+        });
 });
+
+/**
+ * callback for term search autocomplete
+ *
+ * @param {type} search term
+ * @param {type} callback
+ * @returns {obj} json object with labels and values
+ */
+function advads_term_search(field, callback) {
+
+	// return ['post', 'poster'];
+	var query = {
+            action: 'advads-terms-search',
+	};
+
+	query.search = field.value;
+	query.tax = field.dataset.tagName;
+
+	var querying = true;
+
+	var results = {};
+	jQuery.post(ajaxurl, query, function (r) {
+		querying = false;
+		var results = [];
+		if(r){
+			r.map(function(element, index){
+				results[index] = {
+                                    value: element.term_id,
+                                    label: element.name
+				};
+			});
+		}
+		callback( results );
+	}, 'json');
+}
 
 /**
  * callback for post search autocomplete
@@ -107,34 +225,30 @@ jQuery(document).ready(function ($) {
  */
 function advads_post_search(query, callback) {
 
-    // return ['post', 'poster'];
-    var query = {
-        action: 'wp-link-ajax',
-        _ajax_linking_nonce: jQuery('#_ajax_linking_nonce').val()
-    };
+	// return ['post', 'poster'];
+	var query = {
+		action: 'wp-link-ajax',
+		_ajax_linking_nonce: jQuery( '#_ajax_linking_nonce' ).val()
+	};
 
-    query.search = jQuery('#advads-display-conditions-individual-post').val();
+	query.search = jQuery( '#advads-display-conditions-individual-post' ).val();
 
-    var querying = true;
+	var querying = true;
 
-    var results = {};
-    jQuery.post(ajaxurl, query, function (r) {
-        querying = false;
-        var results = [];
-        if(r){
-            r.map(function(element, index){
-                results[index] = {
-                    label: element.title,
-                    value: element.ID
-                };
-            });
-        }
-        callback(results);
-    }, 'json');
-
-//        results = [{huch:'post', hach:'poster', hoch:'position'}];
-    //return results;
-
+	var results = {};
+	jQuery.post(ajaxurl, query, function (r) {
+		querying = false;
+		var results = [];
+		if(r){
+			r.map(function(element, index){
+				results[index] = {
+					label: element.title,
+					value: element.ID
+				};
+			});
+		}
+		callback( results );
+	}, 'json');
 }
 
 /**
@@ -143,7 +257,7 @@ function advads_post_search(query, callback) {
  * @param selector jquery selector
  */
 function advads_toggle(selector) {
-    jQuery(selector).slideToggle();
+	jQuery( selector ).slideToggle();
 }
 
 /**
@@ -152,11 +266,11 @@ function advads_toggle(selector) {
  * @param selector jquery selector
  */
 function advads_toggle_box(e, selector) {
-    if (jQuery(e).is(':checked')) {
-        jQuery(selector).slideDown();
-    } else {
-        jQuery(selector).slideUp();
-    }
+	if (jQuery( e ).is( ':checked' )) {
+		jQuery( selector ).slideDown();
+	} else {
+		jQuery( selector ).slideUp();
+	}
 }
 
 /**
@@ -166,11 +280,11 @@ function advads_toggle_box(e, selector) {
  * @param selector jquery selector
  */
 function advads_toggle_box_enable(e, selector) {
-    if (jQuery(e).is(':checked')) {
-        jQuery(selector).find('input').removeAttr('disabled', '');
-    } else {
-        jQuery(selector).find('input').attr('disabled', 'disabled');
-    }
+	if (jQuery( e ).is( ':checked' )) {
+		jQuery( selector ).find( 'input' ).removeAttr( 'disabled', '' );
+	} else {
+		jQuery( selector ).find( 'input' ).attr( 'disabled', 'disabled' );
+	}
 }
 
 /**
@@ -178,11 +292,11 @@ function advads_toggle_box_enable(e, selector) {
  * @param {bool} value
  */
 function advads_toggle_display_conditions(value) {
-    if (value == 1) {
-        jQuery('#advanced-ad-conditions').fadeIn();
-    } else {
-        jQuery('#advanced-ad-conditions').fadeOut();
-    }
+	if (value == 1) {
+		jQuery( '#advanced-ad-conditions' ).fadeIn();
+	} else {
+		jQuery( '#advanced-ad-conditions' ).fadeOut();
+	}
 }
 
 /**
@@ -190,12 +304,12 @@ function advads_toggle_display_conditions(value) {
  * @param {string} checkbox element
  */
 function advads_toggle_single_display_conditions(checkbox) {
-    // console.log(jQuery(checkbox).parent('div').find('label:not(.advads-conditions-all) input').css('border', 'solid'));
-    if (jQuery(checkbox).is(':checked')) {
-        jQuery(checkbox).parents('.advanced-ad-display-condition').find('.advads-conditions-single').addClass('disabled').find('input').attr('disabled', 'disabled');
-    } else {
-        jQuery(checkbox).parents('.advanced-ad-display-condition').find('.advads-conditions-single').removeClass('disabled').find('input').removeAttr('disabled');
-    }
+	// console.log(jQuery(checkbox).parent('div').find('label:not(.advads-conditions-all) input').css('border', 'solid'));
+	if (jQuery( checkbox ).is( ':checked' )) {
+		jQuery( checkbox ).parents( '.advanced-ad-display-condition' ).find( '.advads-conditions-single' ).addClass( 'disabled' ).find( 'input' ).attr( 'disabled', 'disabled' );
+	} else {
+		jQuery( checkbox ).parents( '.advanced-ad-display-condition' ).find( '.advads-conditions-single' ).removeClass( 'disabled' ).find( 'input' ).removeAttr( 'disabled' );
+	}
 }
 
 /**
@@ -203,13 +317,17 @@ function advads_toggle_single_display_conditions(checkbox) {
  * @param {string} checkbox element
  */
 function advads_toggle_single_display_condition_checkbox(checkbox) {
-    // get the counterpart (same value, but not current element)
-    var counterpart = jQuery(checkbox).parents('.advads-conditions-single').find('input[type="checkbox"][value="' + checkbox.value + '"]').not(checkbox);
-    // toggle counterpart
-    if (jQuery(checkbox).is(':checked')) {
-        counterpart.attr('checked', false);
-        counterpart.attr('disabled', 'disabled');
-    } else {
-        counterpart.removeAttr('disabled');
-    }
+	// get the counterpart (same value, but not current element)
+	var counterpart = jQuery( checkbox ).parents( '.advads-conditions-single' ).find( 'input[type="checkbox"][value="' + checkbox.value + '"]' ).not( checkbox );
+	// toggle counterpart
+	if (jQuery( checkbox ).is( ':checked' )) {
+		counterpart.attr( 'checked', false );
+		counterpart.attr( 'disabled', 'disabled' );
+                // mark label
+                jQuery('label[for="'+counterpart.attr('id')+'"]').addClass('ui-button-disabled').addClass('ui-state-disabled');
+	} else {
+		counterpart.removeAttr( 'disabled' );
+                jQuery('label[for="'+counterpart.attr('id')+'"]').removeClass('ui-button-disabled');
+                jQuery('label[for="'+counterpart.attr('id')+'"]').removeClass('ui-state-disabled');
+	}
 }
---
+++ classes/ad-select.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Abstracts ad selection.
+ *
+ * The class allows to modify 'methods' (named callbacks) to provide ads
+ * through `advanced-ads-ad-select-methods` filter.
+ * This can be used to replace default methods, wrap them or add new ones.
+ *
+ * Further allows to provide ad selection attributes
+ * through `advanced-ads-ad-select-args` filter to influence behaviour of the
+ * selection method.
+ * Default methods have a `override` attribute that allows to replace the
+ * content. This may be used to defer or skip ad codes dynamically.
+ *
+ * @since 1.5.0
+ */
+class Advanced_Ads_Select {
+
+	protected $methods;
+
+	private function __construct() {}
+
+	/**
+	 *
+	 * @var Advanced_Ads_Select
+	 */
+	private static $instance;
+
+	/**
+	 *
+	 * @return Advanced_Ads_Select
+	 */
+	public static function get_instance()
+	{
+		if ( ! isset(self::$instance) ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function get_methods()
+	{
+		if ( ! isset($this->methods) ) {
+			$methods = array(
+				'id' => array( $this, 'get_ad_by_id' ),
+				'group' => array( $this, 'get_ad_by_group' ),
+				'placement' => array( $this, 'get_ad_by_placement' ),
+			);
+
+			$this->methods = apply_filters( 'advanced-ads-ad-select-methods', $methods );
+		}
+
+		return $this->methods;
+	}
+
+	/**
+	 * Advanced ad selection methods should not directly rely on
+	 * current environment factors.
+	 * Prior to actual ad selection the meta is provided to allow for
+	 * serialised, proxied or otherwise defered selection workflows.
+	 *
+	 * @return array
+	 */
+	public function get_ad_arguments( $method, $id, $args = array() )
+	{
+		$args = (array) $args;
+
+		if ( ! isset($args['method']) ) $args['method'] = $method;
+		if ( ! isset($args['id']) ) $args['id'] = $id;
+
+		$args = apply_filters( 'advanced-ads-ad-select-args', $args );
+
+		return $args;
+	}
+
+	public function get_ad_by_method( $id, $method, $args = array() ) {
+
+		$methods = $this->get_methods();
+		if ( ! isset($methods[ $method ]) ) {
+			return ;
+		}
+		if ( ! advads_can_display_ads() ) {
+			return ;
+		}
+		$args = $this->get_ad_arguments( $method, $id, $args );
+
+		return call_user_func( $methods[ $method ], $args );
+	}
+
+	// internal
+	public function get_ad_by_id($args) {
+		if ( ! isset($args['id']) || $args['id'] == 0 ) {
+			return isset($args['override']) ? $args['override'] : null;
+		}
+
+		// get ad
+		$ad = new Advads_Ad( (int) $args['id'], $args );
+
+		// check conditions
+		if ( $ad->can_display() ) {
+			return $ad->output();
+		}
+	}
+
+	// internal
+	public function get_ad_by_group($args) {
+		if ( ! isset($args['id']) || $args['id'] == 0 ) {
+			return isset($args['override']) ? $args['override'] : null;
+		}
+
+		// get ad
+		$adgroup = new Advads_Ad_Group( (int) $args['id'] );
+		return $adgroup->output();
+	}
+
+	// internal
+	public function get_ad_by_placement($args) {
+		if ( ! isset($args['id']) || $args['id'] == '' ) {
+			return isset($args['override']) ? $args['override'] : null;
+		}
+
+		// get placement content
+		return Advads_Ad_Placements::output( $args['id'] );
+	}
+}
